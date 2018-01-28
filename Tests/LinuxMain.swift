fb48d457f855e1ce0973c9357186e1aca72c9cc7
glenn-golden <glenn_golden@apple.com>
2018-01-28 16:00:31 -0500

    swift package init

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import MagicTests
+
+XCTMain([
+    testCase(MagicTests.allTests),
+])
---
+++ Tests/MagicTests/MagicTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Magic
+
+class MagicTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Magic().text, "Hello, World!")
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
